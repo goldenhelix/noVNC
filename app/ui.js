7a193dc6d3e456c81a6cda7525fe256517f479ee
Gabe Rudy <rudy@goldenhelix.com>
2026-05-02 09:42:46 -0600

    Soften disconnect message: "The connection is closed"
    Container shutdown is the normal way for a user to end a session, so
    the previous "Something went wrong, connection is closed" was
    needlessly alarming. Drop the "Something went wrong" prefix and just
    say what happened.
    
    (cherry picked from commit 2c618884e0aeada7ffe207b135965e5bb1e9c9b7)

--- app/ui.js
+++ app/ui.js
@@ -2295,7 +2295,7 @@ const UI = {
         } else if (!e.detail.clean) {
             UI.updateVisualState('disconnected');
             if (wasConnected) {
-                UI.showStatus(_("Something went wrong, connection is closed"),
+                UI.showStatus(_("The connection is closed"),
                               'error');
             } else {
                 UI.showStatus(_("Failed to connect to server"), 'error');
